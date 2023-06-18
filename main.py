17c42395757e890c1d0eb6da1e465d0127304813
Will Larson <lethain@gmail.com>
2023-06-18 09:34:37 -0700

    Continued

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
-df = pd.DataFrame({
-      'first column': [1, 2, 3, 4],
-      'second column': [10, 20, 30, 40]
-    })
 
-df
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+})
+
+st.text_input("Your name", key="name")
+name = st.session_state.name
+st.write(f"This table is created by {name}:")
+
+
+st.line_chart(df)
+st.dataframe(df.style.highlight_max(axis=0))
