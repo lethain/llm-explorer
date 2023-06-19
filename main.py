9f175355dfb06ac217dbf6a5af0d31f26e2a0b12
Will Larson <lethain@gmail.com>
2023-06-18 17:40:32 -0700

    Add model selection

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from oai_utils import get_openai_api_key
 openai_api_key = get_openai_api_key()
 
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+def generate_response(input_text, model_name):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key, model_name=model_name)
     st.info(llm(input_text))
 
 
@@ -17,8 +17,13 @@ st.sidebar.markdown("# LLM Notebook")
 
 
 with st.form('my_form'):
+    oai_model = st.selectbox(
+        'Which OpenAI model should we use?',
+        ('gpt-3.5-turbo', 'gpt-4', 'ada', 'babbage', 'curie', 'davinci'),
+    )
+    
     text = st.text_area('Prompt:', '', placeholder='How do I use Streamlit to query OpenAI?')
     submitted = st.form_submit_button('Submit')
     if submitted:
-        generate_response(text)
+        generate_response(text, oai_model)
 
